7b7cac9a5065f7c19557c7c0c97f121cdd805a26
Prasinos333 <130795819+Prasinos333@users.noreply.github.com>
2024-03-10 01:13:45 -0500

    Restructured to seperate audio files of different type.

--- src/config.js
+++ src/config.js
@@ -3,28 +3,28 @@ import path from "path";
 export const Default_Lang = "en_1";
 
 export const AUDIO = (lang = 'en_1') => ({
-    '10_second_countdown': path.resolve('src', 'resources', lang, '10_second_countdown.mp3'), // 10.916s
-    '15_seconds': path.resolve('src', 'resources', lang, '15_seconds.mp3'), // .957s
-    '20_seconds': path.resolve('src', 'resources', lang, '20_seconds.mp3'), // .761s
-    '30_seconds': path.resolve('src', 'resources', lang, '30_seconds.mp3'), // .745s
-    '40_seconds': path.resolve('src', 'resources', lang, '40_seconds.mp3'), // .743s
-    '50_seconds': path.resolve('src', 'resources', lang, '50_seconds.mp3'), // .745s
-    'Buy Start': path.resolve('src', 'resources', lang, 'Buy_phase_start.mp3'), // 2.853s
-    'Buy War_15': path.resolve('src', 'resources', lang, 'Buy_phase_warn_15.mp3'), // 2.443s
-    'Buy Warn_10': path.resolve('src', 'resources', lang, 'Buy_phase_warn_10.mp3'), // 1.991s
-    'Buy_phase_end': path.resolve('src', 'resources', lang, 'Buy_phase_end.mp3'), // 1.092s
-    'R_Skull Warn': path.resolve('src', 'resources', lang, 'Skull_warn_right.mp3'), // 2.406
-    'M_Skull Warn': path.resolve('src', 'resources', lang, 'Skull_warn_mid.mp3'), // 2.288s
-    'L_Skull Warn': path.resolve('src', 'resources', lang, 'Skull_warn_left.mp3'), // 2.376s
-    'R_Skull': path.resolve('src', 'resources', lang, 'Skull_right.mp3'), // 1.197s
-    'M_Skull' : path.resolve('src', 'resources', lang, 'Skull_mid.mp3'), // .971s
-    'L_Skull' : path.resolve('src', 'resources', lang, 'Skull_left.mp3'), // 1.128s
-    'Last_Wave' : path.resolve('src', 'resources', lang, 'Last_wave.mp3'), // 1.269s
-    'Invasion Notice' : path.resolve('src', 'resources', lang, 'Invasion_notice.mp3'), // 2.563s
-    'Invasion Start' : path.resolve('src', 'resources', lang, 'Invasion_start.mp3'), // 1.071s
-    'War Notice' : path.resolve('src', 'resources', lang, 'War_notice.mp3'), // 1.64s
-    'War Start' : path.resolve('src', 'resources', lang, 'War_notice.mp3') // .818s
-}); // TODO - change names to same as timings.
+    '10_second_countdown': path.resolve('src', 'resources', lang, `war`, '10_second_countdown.mp3'), // 10.916s
+    '15_seconds': path.resolve('src', 'resources', lang, `war`, '15_seconds.mp3'), // .957s
+    '20_seconds': path.resolve('src', 'resources', lang, `war`, '20_seconds.mp3'), // .761s
+    '30_seconds': path.resolve('src', 'resources', lang, `war`, '30_seconds.mp3'), // .745s
+    '40_seconds': path.resolve('src', 'resources', lang, `war`, '40_seconds.mp3'), // .743s
+    '50_seconds': path.resolve('src', 'resources', lang, `war`, '50_seconds.mp3'), // .745s
+    'War Notice' : path.resolve('src', 'resources', lang, `war`, 'War_notice.mp3'), // 1.64s
+    'War Start' : path.resolve('src', 'resources', lang, `war`, 'War_notice.mp3'), // .818s
+    'Buy Start': path.resolve('src', 'resources', lang, `invasion`, 'Buy_phase_start.mp3'), // 2.853s
+    'Buy War_15': path.resolve('src', 'resources', lang, `invasion`, 'Buy_phase_warn_15.mp3'), // 2.443s
+    'Buy Warn_10': path.resolve('src', 'resources', lang, `invasion`, 'Buy_phase_warn_10.mp3'), // 1.991s
+    'Buy_phase_end': path.resolve('src', 'resources', lang, `invasion`, 'Buy_phase_end.mp3'), // 1.092s
+    'R_Skull Warn': path.resolve('src', 'resources', lang, `invasion`, `invasion`, 'Skull_warn_right.mp3'), // 2.406
+    'M_Skull Warn': path.resolve('src', 'resources', lang, `invasion`, 'Skull_warn_mid.mp3'), // 2.288s
+    'L_Skull Warn': path.resolve('src', 'resources', lang, `invasion`, 'Skull_warn_left.mp3'), // 2.376s
+    'R_Skull': path.resolve('src', 'resources', lang, `invasion`, 'Skull_right.mp3'), // 1.197s
+    'M_Skull' : path.resolve('src', 'resources', lang, `invasion`, 'Skull_mid.mp3'), // .971s
+    'L_Skull' : path.resolve('src', 'resources', lang, `invasion`, 'Skull_left.mp3'), // 1.128s
+    'Last_Wave' : path.resolve('src', 'resources', lang, `invasion`, 'Last_wave.mp3'), // 1.269s
+    'Invasion Notice' : path.resolve('src', 'resources', lang, `invasion`, 'Invasion_notice.mp3'), // 2.563s
+    'Invasion Start' : path.resolve('src', 'resources', lang, `invasion`, 'Invasion_start.mp3'), // 1.071s
+}); 
 
 export const InvasionTimings = [ 
     { name: "Buy Start" , value: 1375 },    // 22:55 Buy Phase 1 Start 
@@ -71,84 +71,84 @@ export const InvasionTimings = [
 ];
 
 export const Respawns = [
-    1779,   // 29:39
-    1759,   // 29:19
-    1739,   // 28:59
-    1719,   // 28:39
-    1699,   // 28:19
-    1679,   // 27:59
-    1659,   // 27:39
-    1639,   // 27:19
-    1619,   // 26:59
-    1599,   // 26:39
-    1579,   // 26:19
-    1559,   // 25:59
-    1539,   // 25:39
-    1519,   // 25:19
-    // 1499,    // 24:59
-    1491,   // 24:51
-    1479,   // 24:39
-    // 1463,    // 24:23
-    1455,   // 24:15
-    // 1435,    // 23:55
-    1427,   // 23:47
-    1415,   // 23:35
-    // 1407,    // 23:27
-    1399,   // 23:19
-    // 1379,    // 22:59
-    1371,   // 22:51
-    // 1351,    // 22:31
-    1343,   // 22:23
-    // 1331,    // 22:11
-    1323,   // 22:03
-    // 1303,    // 21:43
-    1295,   // 21:35
-    // 1275,    // 21:15
-    1267,   // 21:07
-    // 1247,    // 20:47
-    1238,   // 20:38
-    // 1219,    // 20:19
-    1211,   // 20:11
-    // 1191,    // 19:51
-    1183,   // 19:43
-    // 1155,    // 19:15
-    1147,   // 19:07
-    // 1119,    // 18:39
-    1111,   // 18:31
-    // 1083,    // 18:03
-    1075,   // 17:55
-    // 1047,    // 17:27
-    1039,   // 17:19
-    // 1011,    // 16:51
-    1003,   // 16:43
-    // 975,     // 16:15
-    967,    // 16:07
-    // 939,     // 15:39
-    931,    // 15:31
-    // 903,     // 15:03
-    895,    // 14:55
-    // 867,     // 14:27
-    859,    // 14:19
-    // 823,     // 13:43
-    815,    // 13:35
-    // 779,     // 12:59
-    771,    // 12:51
-    // 735,     // 12:15
-    727,    // 12:07
-    // 691,     // 11:31
-    683,    // 11:23
-    // 647,     // 10:47
-    639,    // 10:39
-    // 603,     // 10:03
-    595,    // 09:55
-    551,    // 09:11
-    499,    // 08:19
-    447,    // 07:27
-    395,    // 06:35
-    343,    // 05:43
-    291,    // 04:51
-    231,    // 03:51
-    171,    // 02:51
-    111,    // 01:51
-    51      // 00:51
+    { wave: 0, value: 1779 },   // 29:39
+    { wave: 0, value: 1759 },   // 29:19
+    { wave: 0, value: 1739 },   // 28:59
+    { wave: 0, value: 1719 },   // 28:39
+    { wave: 0, value: 1699 },   // 28:19
+    { wave: 0, value: 1679 },   // 27:59
+    { wave: 0, value: 1659 },   // 27:39
+    { wave: 0, value: 1639 },   // 27:19
+    { wave: 0, value: 1619 },   // 26:59
+    { wave: 0, value: 1599 },   // 26:39
+    { wave: 0, value: 1579 },   // 26:19
+    { wave: 0, value: 1559 },   // 25:59
+    { wave: 0, value: 1539 },   // 25:39
+    { wave: 0, value: 1519 },   // 25:19
+    { wave: 2, value: 1499 },   // 24:59
+    { wave: 1, value: 1491 },   // 24:51
+    { wave: 1, value: 1479 },   // 24:39
+    { wave: 2, value: 1463 },   // 24:23
+    { wave: 1, value: 1455 },   // 24:15
+    { wave: 2, value: 1435 },   // 23:55
+    { wave: 1, value: 1427 },   // 23:47
+    { wave: 1, value: 1415 },   // 23:35  
+    { wave: 2, value: 1407 },   // 23:27
+    { wave: 1, value: 1399 },   // 23:19
+    { wave: 2, value: 1379 },   // 22:59
+    { wave: 1, value: 1371 },   // 22:51
+    { wave: 2, value: 1351 },   // 22:31
+    { wave: 1, value: 1343 },   // 22:23
+    { wave: 2, value: 1331 },   // 22:11
+    { wave: 1, value: 1323 },   // 22:03
+    { wave: 2, value: 1303 },   // 21:43
+    { wave: 1, value: 1295 },   // 21:35
+    { wave: 2, value: 1275 },   // 21:15
+    { wave: 1, value: 1267 },   // 21:07
+    { wave: 2, value: 1247 },   // 20:47
+    { wave: 1, value: 1238 },   // 20:38
+    { wave: 2, value: 1219 },   // 20:19
+    { wave: 1, value: 1211 },   // 20:11
+    { wave: 2, value: 1191 },   // 19:51
+    { wave: 1, value: 1183 },   // 19:43
+    { wave: 2, value: 1155 },   // 19:15
+    { wave: 1, value: 1147 },   // 19:07
+    { wave: 2, value: 1119 },   // 18:39
+    { wave: 1, value: 1111 },   // 18:31   
+    { wave: 2, value: 1083 },   // 18:03
+    { wave: 1, value: 1075 },   // 17:55
+    { wave: 2, value: 1047 },   // 17:27
+    { wave: 1, value: 1039 },   // 17:19
+    { wave: 2, value: 1011 },   // 16:51
+    { wave: 1, value: 1003 },   // 16:43
+    { wave: 2, value: 975 },    // 16:15
+    { wave: 1, value: 967 },    // 16:07
+    { wave: 2, value: 939 },    // 15:39
+    { wave: 1, value: 931 },    // 15:31
+    { wave: 2, value: 903 },    // 15:03
+    { wave: 1, value: 895 },    // 14:55
+    { wave: 2, value: 867 },    // 14:27
+    { wave: 1, value: 859 },    // 14:19
+    { wave: 2, value: 823 },    // 13:43
+    { wave: 1, value: 815 },    // 13:35
+    { wave: 2, value: 779 },    // 12:59
+    { wave: 1, value: 771 },    // 12:51
+    { wave: 2, value: 735 },    // 12:15
+    { wave: 1, value: 727 },    // 12:07
+    { wave: 2, value: 691 },    // 11:31
+    { wave: 1, value: 683 },    // 11:23
+    { wave: 2, value: 647 },    // 10:47
+    { wave: 1, value: 639 },    // 10:39       
+    { wave: 2, value: 603 },    // 10:03
+    { wave: 0, value: 595 },    // 09:55 
+    { wave: 0, value: 551 },    // 09:11 
+    { wave: 0, value: 499 },    // 08:19 
+    { wave: 0, value: 447 },    // 07:27 
+    { wave: 0, value: 395 },    // 06:35 
+    { wave: 0, value: 343 },    // 05:43 
+    { wave: 0, value: 291 },    // 04:51 
+    { wave: 0, value: 231 },    // 03:51 
+    { wave: 0, value: 171 },    // 02:51 
+    { wave: 0, value: 111 },    // 01:51
+    { wave: 0, value: 51 },     // 00:51   
 ];
