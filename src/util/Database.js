997da2c2faf658db5fc407a9a6c19484c9a6481e
Prasinos333 <130795819+Prasinos333@users.noreply.github.com>
2024-03-19 00:28:48 -0400

    Updated database query

--- src/util/Database.js
+++ src/util/Database.js
@@ -15,10 +15,10 @@ class Database {
     async retrieveConfig(userID)  {
         return new Promise((resolve, reject) => {
             this.connection.query(
-                `SELECT Lang, Type FROM InvasionConfig WHERE UserID = ${ userID }`,
+                `SELECT Lang, Setting FROM InvasionConfig WHERE UserID = ${ userID }`,
                 (err, results) => {
                     if (err) reject(err);
-                    else resolve(results[0]);
+                    else resolve(results);
                 }
             );
         });
@@ -27,7 +27,7 @@ class Database {
     async addConfig(userID, lang, type) {
         return new Promise((resolve, reject) => {
             this.connection.query(
-                `INSERT INTO InvasionConfig (UserID, Lang, Type) VALUES (${ userID }, ${ lang } , ${ type }) ON DUPLICATE KEY UPDATE Lang = VALUES(Lang), Type = VALUES(Type)`,
+                `INSERT INTO InvasionConfig (UserID, Lang, Setting) VALUES (${ userID }, ${ lang } , ${ type }) ON DUPLICATE KEY UPDATE Lang = VALUES(Lang), Type = VALUES(Type)`,
                 (err, results) => {
                     if (err) reject(err);
                     else resolve(results);
