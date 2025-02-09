e88c6132a7547a05054738667ebee9d777b4327e
Prasinos333 <130795819+Prasinos333@users.noreply.github.com>
2025-02-09 18:13:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
-import Bot from "./bots/Bot.js";
+import Bot from "./bots/bot.js";
 import MainBot from "./bots/mainBot.js";
 import Database from "./util/database.js";
 import logger from "./util/logger.js";
