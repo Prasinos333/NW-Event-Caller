7e664d2f983bd934f776b53ac56cd05c917c6df3
Prasinos333 <130795819+Prasinos333@users.noreply.github.com>
2025-04-12 12:08:29 -0400

    Added allowRoles execute and refactored mainBot.js for improved readability and consistency.

--- src/bots/mainBot.js
+++ src/bots/mainBot.js
@@ -1,51 +1,56 @@
 import Bot from "./basicBot.js";
-import { execute as voiceraffleExecute } from "../commands/voiceRaffle.js"
-import { execute as addcallerExecute } from "../commands/addCaller.js"
-import Discord, { ActivityType, GatewayIntentBits } from "discord.js"
+import { execute as voiceraffleExecute } from "../commands/voiceRaffle.js";
+import { execute as addcallerExecute } from "../commands/addCaller.js";
+import { execute as allowRolesExecute } from "../commands/allowRoles.js";
+import Discord, { ActivityType, GatewayIntentBits } from "discord.js";
 
 class MainBot extends Bot {
+  constructor({ name, token }) {
+    super({ name, token });
 
-    constructor({ name, token }) {
-        super({ name, token });
+    this.client = new Discord.Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+      ],
+    });
 
-        this.client = new Discord.Client({
-            intents: [
-                GatewayIntentBits.Guilds,
-                GatewayIntentBits.GuildMembers,
-                GatewayIntentBits.GuildMessages,
-                GatewayIntentBits.GuildVoiceStates
-            ]
-        });
+    this.initialise();
+  }
 
-        this.initialise();
-    }
+  /**
+   * Initialises the bot and sets up the event listeners.
+   */
+  initialise = () => {
+    this.client.login(this.token).catch(console.error);
 
-    initialise = () => {
-        this.client.login(this.token)
-            .catch(console.error);
+    this.client.once("ready", () => {
+      this.client.user.setActivity({
+        name: "Slash Commands",
+        type: ActivityType.Listening,
+      });
+    });
 
-        this.client.once('ready', () => {
-            
-            this.client.user.setActivity({
-                name: "Slash Commands",
-                type: ActivityType.Listening
-            });
-        });
+    this.client.on("interactionCreate", async (interaction) => {
+      if (!interaction.isCommand()) return;
 
-        this.client.on('interactionCreate', async interaction => { 
-            if (!interaction.isCommand()) return;
-        
-            const { commandName } = interaction;
-        
-            if (commandName === 'addcaller') { 
-                await addcallerExecute(interaction);
-            }
-        
-            if(commandName === 'voiceraffle') { 
-                await voiceraffleExecute(interaction);
-            }
-        });
-    }
+      const { commandName } = interaction;
+
+      if (commandName === "addcaller") {
+        await addcallerExecute(interaction);
+      }
+
+      if (commandName === "voiceraffle") {
+        await voiceraffleExecute(interaction);
+      }
+
+      if (commandName === "allowroles") {
+        await allowRolesExecute(interaction);
+      }
+    });
+  };
 }
 
 export default MainBot;
