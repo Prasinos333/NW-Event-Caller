a0ddb0b79b30527353e56d35f32ea01fb515dd24
Prasinos333 <130795819+Prasinos333@users.noreply.github.com>
2025-04-12 12:05:28 -0400

    Refactor button and select menu exports for improved readability and consistency

--- src/util/buttons.js
+++ src/util/buttons.js
@@ -1,42 +1,46 @@
-import { ButtonBuilder, ButtonStyle, StringSelectMenuBuilder } from 'discord.js';
-import { invasionOptions, invasionVoices, warVoices } from '../config.js';
+import {
+  ButtonBuilder,
+  ButtonStyle,
+  StringSelectMenuBuilder,
+} from "discord.js";
+import { invasionOptions, invasionVoices, warVoices } from "../config.js";
 
 export const stopButton = new ButtonBuilder()
-                .setCustomId('stop')
-                .setStyle(ButtonStyle.Danger)
-                .setEmoji('⏹️');
+  .setCustomId("stop")
+  .setStyle(ButtonStyle.Danger)
+  .setEmoji("⏹️");
 
-export const muteButton = new ButtonBuilder()
-                .setCustomId('mute')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji('🔇');
-
-export const unmuteButton = new ButtonBuilder()
-                .setCustomId('unmute')
-                .setStyle(ButtonStyle.Success)
-                .setEmoji('🔊');
+export const langButton = new ButtonBuilder()
+  .setCustomId("lang")
+  .setStyle(ButtonStyle.Secondary)
+  .setEmoji("🌐");
 
 export const settingsButton = new ButtonBuilder()
-                .setCustomId('settings')
-                .setStyle(ButtonStyle.Primary)
-                .setEmoji('⚙️');
+  .setCustomId("settings")
+  .setStyle(ButtonStyle.Primary)
+  .setEmoji("⚙️");
 
 export const invasionSelect = new StringSelectMenuBuilder()
-                .setCustomId('select')
-                .setPlaceholder('Change Voice')
-                .addOptions(invasionVoices);
-
-export const warSelect = new StringSelectMenuBuilder()
-                .setCustomId('select')
-                .setPlaceholder('Change Voice')
-                .addOptions(warVoices);
+  .setCustomId("select")
+  .setPlaceholder("Select a voice")
+  .addOptions(invasionVoices);
 
 export const invasionSettings = new StringSelectMenuBuilder()
-                .setCustomId('setting')
-                .setPlaceholder('Timings to be called')
-                .addOptions(invasionOptions)
-                .setMinValues(1)
-                .setMaxValues(4);
+  .setCustomId("options")
+  .setPlaceholder("Timings to be called")
+  .addOptions(invasionOptions)
+  .setMinValues(1)
+  .setMaxValues(4);
 
+export const warSelect = new StringSelectMenuBuilder()
+  .setCustomId("select")
+  .setPlaceholder("Select a voice")
+  .addOptions(warVoices);
 
-    
+export const waveButton = new ButtonBuilder()
+  .setCustomId("wave_switch")
+  .setLabel("Switch Wave")
+  .setEmoji("🔄")
+  .setStyle(ButtonStyle.Secondary);
+
+export const blankField = { name: "\u200B", value: "\u200B", inline: true }; // Empty field for embed
