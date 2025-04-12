382fa2162e0c6defec2a30e3fa2e8996fc44d8c6
Prasinos333 <130795819+Prasinos333@users.noreply.github.com>
2025-04-12 12:04:55 -0400

    Refactor logger and slashCommands for improved readability and consistency

--- src/util/logger.js
+++ src/util/logger.js
@@ -2,23 +2,39 @@ import { Console } from "console";
 import fs from "fs";
 
 export default (path) => {
-    const logger = new Console({
-        stdout: fs.createWriteStream(path, { flags: 'a' })
-    });
+  const logger = new Console({
+    stdout: fs.createWriteStream(path, { flags: "a" }),
+  });
 
-    const overwrite = [
-        [logger.log, (message) => `[${ new Date().toLocaleString('en-US', { timeZone: "America/New_York", timeZoneName: 'short'}) }] [LOG] ${ message }`],
-        [logger.info, (message) => `[${ new Date().toLocaleString('en-US', { timeZone: "America/New_York", timeZoneName: 'short'}) }] [INFO] ${ message }`],
-        [logger.warn, (message) => `[${ new Date().toLocaleString('en-US', { timeZone: "America/New_York", timeZoneName: 'short'}) }] [WARN] ${ message }`],
-        [logger.error, (message) => `[${ new Date().toLocaleString('en-US', { timeZone: "America/New_York", timeZoneName: 'short'}) }] [ERROR] ${ message }`]
-    ];
+  const overwrite = [
+    [
+      logger.log,
+      (message) =>
+        `[${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeZoneName: "short" })}] [LOG] ${message}`,
+    ],
+    [
+      logger.info,
+      (message) =>
+        `[${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeZoneName: "short" })}] [INFO] ${message}`,
+    ],
+    [
+      logger.warn,
+      (message) =>
+        `[${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeZoneName: "short" })}] [WARN] ${message}`,
+    ],
+    [
+      logger.error,
+      (message) =>
+        `[${new Date().toLocaleString("en-US", { timeZone: "America/New_York", timeZoneName: "short" })}] [ERROR] ${message}`,
+    ],
+  ];
 
-    overwrite.forEach(([ _function, _format ]) => {
-        logger[_function.name] = function() {
-            arguments[0] = _format(arguments[0]);
-            _function.apply(this, arguments);
-        }
-    });
+  overwrite.forEach(([_function, _format]) => {
+    logger[_function.name] = function () {
+      arguments[0] = _format(arguments[0]);
+      _function.apply(this, arguments);
+    };
+  });
 
-    return logger;
-}
+  return logger;
+};
